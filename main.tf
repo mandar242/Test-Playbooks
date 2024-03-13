e1a52f4fc955707d0ad7fabab7a8767f1c186203
Mandar Kulkarni <mandar242@gmail.com>
2024-03-12 18:20:03 -0700

    role-test = module_plan_stash_role = add example playbooks

---
+++ main.tf
@@ -0,0 +1,14 @@
+# Config to generate random string
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.6.0"
+    }
+  }
+}
+
+resource "random_string" "random" {
+  length           = 16
+  special          = true
+}
