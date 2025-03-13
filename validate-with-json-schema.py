da0ab455012d13278c0bf4f57502eefceab3db4d
Mandar Kulkarni <mandar242@gmail.com>
2025-03-13 13:10:15 -0700

    Create validate-with-json-schema.py

---
+++ validate-with-json-schema.py
@@ -0,0 +1,18 @@
+import yaml
+import jsonschema
+
+def main():
+    # Load the schema
+    with open("schema.yaml", "r") as schema_file:
+        schema_dict = yaml.safe_load(schema_file)
+
+    # Load the data to validate against schema
+    with open("data.yaml", "r") as data_file:
+        data_dict = yaml.safe_load(data_file)
+
+    # Validate using the JSON Schema library
+    jsonschema.validate(instance=data_dict, schema=schema_dict)
+    print("Validation successful!")
+
+if __name__ == "__main__":
+    main()
